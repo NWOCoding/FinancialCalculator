f933c6be21aeb413ac5049605b065d8a5ecaa1e4
OctOcat <willierivas97@gmail.com>
2023-04-14 15:30:17 -0500

    created new project

--- src/main/java/org/yearup/Main.java
+++ src/main/java/org/yearup/Main.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class Main {
    public static void main (String[] args) {
-    Scanner sc = new Scanner(System.in);
+      Scanner sc = new Scanner(System.in);
 
 
-    //Input
-       System.out.print("Enter the amount borrowed: ");
-       double principal = sc.nextDouble();
-       System.out.print("Enter the annual interest rate: ");
-       double interestRate = sc.nextDouble();
-       System.out.print("Enter the loan term in years");
-       double loanTerm = sc.nextDouble();
+      //Input
+      System.out.print("Enter the amount borrowed: ");
+      double principal = sc.nextDouble();
+      System.out.print("Enter the annual interest rate: ");
+      double interestRate = sc.nextDouble();
+      System.out.print("Enter the loan term in years; ");
+      double loanTerm = sc.nextDouble();
 
 
-       //Convert interest rate from annual to monthly
-       interestRate /= 1200;
-       loanTerm *= 12;
+      //Convert interest rate from annual to monthly
+      interestRate /= 1200;
+      loanTerm *= 12;
 
-       //calculate monthly payment
-       double monthlyPayment=(principal * interestRate) / (1-Math.pow(1 + interestRate, -loanTerm));
+      //calculate monthly payment
+      double monthlyPayment = (principal * interestRate) / (1 - Math.pow(1 + interestRate, -loanTerm));
 
 
-
+   }
 }
