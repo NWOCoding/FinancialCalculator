fd947f1496babe27e60e74543533ec02ddf8f22f
OctOcat <willierivas97@gmail.com>
2023-04-14 16:33:57 -0500

    created new project

--- src/main/java/org/yearup/Main.java
+++ src/main/java/org/yearup/Main.java
@@ -4,39 +4,30 @@ import java.security.Principal;
 import java.util.Scanner;
 
 public class Main {
-   public static void main (String[] args) {
-      Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
 
-      //Input
-      System.out.print("Enter the Loan amount: ");
-      double principal = sc.nextDouble();
-      System.out.print("Enter the annual interest rate: ");
-      double annualInterestRate = sc.nextDouble();
-      System.out.print("Enter the loan term in years; ");
-      Double loanTermInYears = sc.nextDouble();
+        //Input
+        System.out.print("Enter the Loan amount: ");
+        double principal = sc.nextDouble();
+        System.out.print("Enter the annual interest rate: ");
+        double annualInterestRate = sc.nextDouble();
+        System.out.print("Enter the loan term in years; ");
+        Double loanTermInYears = sc.nextDouble();
 
 
-      //Calculations
-      Double totalNumberOfPayments = loanTermInYears * 12;
-      double monthlyInterestRate = annualInterestRate / 1200;
-      double monthlyPayment = (principal* monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -totalNumberOfPayments));
-      double totalInterestPaid = monthlyPayment * totalNumberOfPayments - principal;
+        //Calculations
+        Double totalNumberOfPayments = loanTermInYears * 12;
+        double monthlyInterestRate = annualInterestRate / 1200;
+        double monthlyPayment = (principal * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -totalNumberOfPayments));
+        double totalInterestPaid = monthlyPayment * totalNumberOfPayments - principal;
 
 
+        //Display results
+        System.out.printf("Monthly payment: $%.2f\n", monthlyPayment);
+        System.out.printf("Total interest paid: $%.2f\n", totalInterestPaid);
 
 
-      //Display results
-      System.out.printf("Monthly payment: $%.2f\n", monthlyPayment);
-      System.out.printf("Total interest paid: $%.2f\n", totalInterestPaid);
-
-
-
-
-
-
-
-
-
-   }
+    }
 }
