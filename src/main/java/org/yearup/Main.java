69051c413b2e2608c64cc0de39a6fab00108a0d1
OctOcat <willierivas97@gmail.com>
2023-04-14 16:30:02 -0500

    created new project

--- src/main/java/org/yearup/Main.java
+++ src/main/java/org/yearup/Main.java
@@ -1,5 +1,6 @@
 package org.yearup;
 
+import java.security.Principal;
 import java.util.Scanner;
 
 public class Main {
@@ -8,20 +9,33 @@ public class Main {
 
 
       //Input
-      System.out.print("Enter the amount borrowed: ");
+      System.out.print("Enter the Loan amount: ");
       double principal = sc.nextDouble();
       System.out.print("Enter the annual interest rate: ");
-      double interestRate = sc.nextDouble();
+      double annualInterestRate = sc.nextDouble();
       System.out.print("Enter the loan term in years; ");
-      double loanTerm = sc.nextDouble();
+      Double loanTermInYears = sc.nextDouble();
 
 
       //Convert interest rate from annual to monthly
-      interestRate /= 1200;
-      loanTerm *= 12;
+      Double totalNumberOfPayments = loanTermInYears * 12;
+      double monthlyInterestRate = annualInterestRate / 1200;
+      double monthlyPayment = (principal* monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -totalNumberOfPayments));
+      double totalInterestPaid = monthlyPayment * totalNumberOfPayments - principal;
 
-      //calculate monthly payment
-      double monthlyPayment = (principal * interestRate) / (1 - Math.pow(1 + interestRate, -loanTerm));
+
+
+
+      //Display results
+      System.out.printf("Monthly payment: $%.2f\n", monthlyPayment);
+      System.out.printf("Total interest paid: $%.2f\n", totalInterestPaid);
+
+
+
+
+
+
+
 
 
    }
